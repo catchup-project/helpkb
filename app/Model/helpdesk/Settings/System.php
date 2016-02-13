0f84c1a08726ac185a5ef6f564f546815eaadb70
Niels Drost <nielsdrost7@gmail.com>
2016-02-13 23:04:48 +0100

    Commit it all

--- app/Model/helpdesk/Settings/System.php
+++ app/Model/helpdesk/Settings/System.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class System extends Model {
+class System extends Model
+{
 
-	/* Using System Table */
-	protected $table = 'settings_system';
-	protected $fillable = [
+  /* Using System Table */
+  protected $table = 'settings_system';
+  protected $fillable = [
 
-							'id','status','url','name','department','page_size','log_level','purge_log','name_format',
-							'time_farmat','date_format','date_time_format','day_date_time','time_zone','content'
-	];
+    'id', 'status', 'url', 'name', 'department', 'page_size', 'log_level', 'purge_log', 'name_format',
+    'time_farmat', 'date_format', 'date_time_format', 'day_date_time', 'time_zone', 'content'
+  ];
 
 }
--- app/Model/kb/Category.php
+++ app/Model/kb/Category.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model {
+class Category extends Model
+{
 
-	protected $table = 'kb_category';
-	protected $fillable = ['id', 'slug', 'name', 'description', 'status', 'parent', 'created_at', 'updated_at'];
+  protected $table = 'kb_category';
+  protected $fillable = ['id', 'slug', 'name', 'description', 'status', 'parent', 'created_at', 'updated_at'];
 
 }
